d3d737f385304020a56dda1732cf57f409fe6a21
wdEffort <yoman3011@gmail.com>
2021-07-20 23:29:04 +0900

    로그인 처리(1) - 로그인 성공 후 처리(쿠키 사용)

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,48 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * @param memberId
+     * @param model
+     * @return
+     * @CookieValue "memberId" 쿠키를 가져오기 위해 사용
+     * 로그인하지 않은 사용자를 위해 required 속성을 false로 설정
+     */
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
     }
 }
