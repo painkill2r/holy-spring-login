fc644a11d819e8aadd23b4e4aca258c0ea75d74b
wdEffort <yoman3011@gmail.com>
2021-07-21 19:00:00 +0900

    로그인 처리(1) - 서블릿 HttpSession을 사용한 로그인 처리

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web;
+
+/**
+ * 객체로 생성할 일이 없도록 추상 클래스로 선언
+ */
+public abstract class SessionConst {
+
+    public static final String LOGIN_MEMBER = "loginMember";
+}
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -68,7 +70,7 @@ public class LoginController {
      * @param response
      * @return
      */
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -91,6 +93,38 @@ public class LoginController {
     }
 
     /**
+     * HttpSession을 사용
+     *
+     * @param form
+     * @param bindingResult
+     * @param request
+     * @return
+     */
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            //글로벌 오류
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리 TODO
+
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:/";
+    }
+
+    /**
      * 쿠키 사용
      *
      * @param response
@@ -108,9 +142,29 @@ public class LoginController {
      * @param request
      * @return
      */
-    @PostMapping("/logout")
+    //@PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    /**
+     * HttpSession을 사용
+     *
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request) {
+        //request.getSession(true): 세션이 있으면 있는 세션을 반환, 세션이 없으면 새로 생성함.
+        //request.getSession(false): 세션이 있으면 있는 세션을 반환, 세션이 없으면 새로 생성하지 않음.
+        //세션을 지울 것이기 때문에 세션이 없더라도 새로 생성하지 않도록 false로 설정
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
         return "redirect:/";
     }
 
