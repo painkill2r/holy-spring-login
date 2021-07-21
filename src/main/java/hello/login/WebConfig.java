58cb1f54bebb9c96ccffd5ba426fc2ce7ff2d1ab
wdEffort <yoman3011@gmail.com>
2021-07-21 22:45:45 +0900

    로그인 처리(2) - 인증 체크 필터 구현 및 설정

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,16 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); //등록할 필터
         filterRegistrationBean.setOrder(1); //순서(낮을 수록 먼저 동작)
+        filterRegistrationBean.addUrlPatterns("/*"); //필터를 적용시킬 서블릿 URL 패턴
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter()); //등록할 필터
+        filterRegistrationBean.setOrder(2); //순서(낮을 수록 먼저 동작)
         filterRegistrationBean.addUrlPatterns("/*"); //필터를 적용시킬 서블릿 URL 패턴
 
         return filterRegistrationBean;
